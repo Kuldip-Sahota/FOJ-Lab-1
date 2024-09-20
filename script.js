04bfd984032d72ea38664bd1a83b1c598e1e0e22
Kuldip-Sahota <kuldipsahota10@gmail.com>
2024-09-20 19:00:10 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+let studentName = "Kuldip Sahota";   
+let studentAge = 18;               
+let isEnrolled = true;             
+
+console.log("Student Name: " + studentName);
+console.log("Student Age: " + studentAge);
+console.log("Is Enrolled: " + isEnrolled);
+
+let text = "Shrek";
+console.log("String: " + text);
+
+let number = 42;
+console.log("Number: " + number);
+
+let isTrue = false;
+console.log("Boolean: " + isTrue);
+
+let fruits = ["apple", "banana", "cherry"];
+console.log("Array: " + fruits);
+
+let student = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 21
+};
+console.log("Object: ", student);
+
+let sum = 5 + 3;
+let difference = 10 - 4;
+let product = 6 * 7;
+let quotient = 20 / 5;
+console.log("Sum: " + sum);
+console.log("Difference: " + difference);
+console.log("Product: " + product);
+console.log("Quotient: " + quotient);
+
+
+console.log("10 > 5: " + (10 > 5));      // true
+console.log("4 < 2: " + (4 < 2));        // false
+console.log("5 === 5: " + (5 === 5));    // true
+console.log("5 !== 3: " + (5 !== 3));    // true
+
+
+console.log("true && false: " + (true && false));  // false
+console.log("true || false: " + (true || false));  // true
+console.log("!true: " + (!true));                  // false
